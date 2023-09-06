2049581e8dffdb0abfd815346936dfa17dcd75d8
vitaliyzh92 <zh.vitaliy92@yandex.ru>
2023-09-06 21:43:46 +0300

    Скрытие не используемой клавиатуры пользователя

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,14 +1,24 @@
-from aiogram import types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-import sqlite3
 import datetime
+import sqlite3
 
-from database.db_manager import recording_user_data
-from keyboards.user_keyboards import welcome_keyboard
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import state
+from loguru import logger
+
+from database.db_manager import recording_user_data, check_user_data_exists
+from keyboards.user_keyboards import welcome_keyboard, contact_keyboard
 from messages.user_messages import message_text
 from system.dispatcher import dp, bot
 
+# Настройка логирования
+logger.add("setting/log/log.log", rotation="1 MB", compression="zip")
 
+# Создаем состояние "ожидание номера телефона"
+class RegistrationState(state.State):
+    phone_number = state.State()  # Здесь можно хранить номер телефона пользователя
+
+# Обработчик команды /start
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
     """Обработчик команды /start"""
@@ -16,46 +26,77 @@ async def start_command(message: types.Message):
     await message.reply(message_text, reply_markup=inline_keyboard)
 
 
-async def contact_keyboard():
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    first_button = KeyboardButton(text="📱 Отправить", request_contact=True)
-    markup.add(first_button)
-    return markup
 
-
-@dp.message_handler(content_types=types.ContentType.CONTACT)
-async def get_contact(message: types.Message):
+# Обработчик для получения контакта
+@dp.message_handler(content_types=types.ContentType.CONTACT, state="*")
+async def get_contact(message: types.Message, state: FSMContext):
     contact = message.contact
 
-    # Получение данных
+    # Получение информации о пользователе
     user_id = message.from_user.id
     username = message.from_user.username
-    number_phone = contact.phone_number
     first_name = message.from_user.first_name
     last_name = message.from_user.last_name
-    date_now = datetime.datetime.now()
 
-    # Записываем данные в базу данных
-    recording_user_data(user_id, username, number_phone, first_name, last_name, date_now)
+    # Проверка наличия данных о пользователе в базе данных на основе user_id
+    user_data_exists = check_user_data_exists(user_id)
 
-    await message.answer(f"Спасибо, {contact.full_name}.\nЗа регистрацию")
+    if user_data_exists:
+        # Данные о пользователе уже существуют, номер телефона не запрашиваем
+        await message.answer(f"Спасибо, {contact.full_name}.\nВы уже зарегистрированы.")
 
-    send_video_text = ("Отправляем вам видео...\n"
-                       "<a href='https://youtu.be/JXRep76T4yU'>Видео 1</a>\n"
-                       "<a href='https://youtu.be/59npEilTIjg'>Видео 2</a>\n\n"
-                       "Для возврата нажмите /start")
-    await bot.send_message(message.chat.id, send_video_text, disable_web_page_preview=True)
+        send_video_text = ("Отправляем вам видео...\n"
+                           "<a href='https://youtu.be/JXRep76T4yU'>Видео 1</a>\n"
+                           "<a href='https://youtu.be/59npEilTIjg'>Видео 2</a>\n\n"
+                           "Для возврата нажмите /start")
+        await bot.send_message(message.chat.id, send_video_text, disable_web_page_preview=True)
+    else:
+        # Данные о пользователе отсутствуют, запрашиваем номер телефона и записываем данные
+        number_phone = contact.phone_number
+        date_now = datetime.datetime.now()
 
+        # Запись данных о пользователе в базу данных
+        recording_user_data(user_id, username, number_phone, first_name, last_name, date_now)
 
+        await message.answer(f"Спасибо, {contact.full_name}.\nЗа регистрацию")
+
+        send_video_text = ("Отправляем вам видео...\n"
+                           "<a href='https://youtu.be/JXRep76T4yU'>Видео 1</a>\n"
+                           "<a href='https://youtu.be/59npEilTIjg'>Видео 2</a>\n\n"
+                           "Для возврата нажмите /start")
+
+        # Удаляем клавиатуру после получения номера
+        await bot.send_message(message.chat.id, send_video_text, reply_markup=types.ReplyKeyboardRemove(),
+                               disable_web_page_preview=True)
+
+        # Переключаем состояние пользователя на "готово"
+        await RegistrationState.phone_number.set()
+
+# Обработчик для кнопки "Получить видео"
 @dp.callback_query_handler(lambda callback_query: callback_query.data == "get_video")
 async def get_video(callback_query: types.CallbackQuery):
     """Ответ на нажатие inline кнопки get_video"""
-    text_message = ("📹 Для получения видео, необходимо пройти короткую регистрацию.\n\n"
-                    "🔐 Регистрация проводится один раз.\n\n"
-                    "<i>Для регистрации, пожалуйста, нажмите кнопку ниже, чтобы отправить контакт. 📱</i>")
-    await bot.send_message(callback_query.message.chat.id, text_message, reply_markup=await contact_keyboard())
 
+    # Получение информации о пользователе
+    user_id = callback_query.from_user.id
 
+    # Проверка наличия данных о пользователе в базе данных на основе user_id
+    user_data_exists = check_user_data_exists(user_id)
+
+    if user_data_exists:
+        # Данные о пользователе уже существуют, сразу отправляем видео
+        send_video_text = ("Отправляем вам видео...\n"
+                           "<a href='https://youtu.be/JXRep76T4yU'>Видео 1</a>\n"
+                           "<a href='https://youtu.be/59npEilTIjg'>Видео 2</a>\n\n"
+                           "Для возврата нажмите /start")
+        await bot.send_message(callback_query.message.chat.id, send_video_text, disable_web_page_preview=True)
+    else:
+        text_message = ("📹 Для получения видео, необходимо пройти короткую регистрацию.\n\n"
+                        "🔐 Регистрация проводится один раз.\n\n"
+                        "<i>Для регистрации, пожалуйста, нажмите кнопку ниже, чтобы отправить контакт. 📱</i>")
+        await bot.send_message(callback_query.message.chat.id, text_message, reply_markup=await contact_keyboard())
+
+# Функция для регистрации обработчиков сообщений
 def greeting_handler():
     """Регистрируем handlers для бота"""
     dp.register_message_handler(start_command)  # Обработчик команды /start, он же пост приветствия
